d58d75d162196adefb18f55c566869a04a962294
thi64146 <thi64146@adobe.com>
2022-04-25 13:44:46 -0600

    simplified headline and added ribbon

--- blocks/headline/headline.js
+++ blocks/headline/headline.js
@@ -49,24 +49,14 @@ const init = (element) => {
     background.classList.add('background');
     const bgHasImg = background.querySelector(':scope img');
     if (!bgHasImg) {
-      const bgColor = background.textContent;
-      element.style.background = bgColor;
+      element.style.background = background.textContent;
       children[0].remove();
     }
 
-    const container = children[1];
+    const container = children[children.length - 1];
     container.classList.add('container');
     decorateText(container);
     decorateButtons(container);
-
-    const ribbon = children[children.length - 1];
-    ribbon.classList.add('ribbon');
-    const bgImg = ribbon.querySelector(':scope img');
-    if (!bgImg) {
-        const bgColor = ribbon.textContent;
-        ribbon.style = `background: ${bgColor}`;
-        ribbon.innerHTML = '';
-    }
 
 };
 
